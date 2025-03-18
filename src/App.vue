0fc1d24ee3613154a51c948f53ef6bfc19908eec
plutus <914185324@qq.com>
2025-03-18 20:45:47 +0800

    key属性添加到v-for

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import HelloWorld from './components/HelloWorld.vue'
 import IfDemo from './components/IfDemo.vue'
 import ListDemo from './components/ListDemo.vue'
+import keyDemo from './components/keyDemo.vue'
 </script>
 
 <template>
   <HelloWorld/>
   <IfDemo/>
   <ListDemo/>
+  <keyDemo/>
 </template>
