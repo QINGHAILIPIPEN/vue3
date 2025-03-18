cf1b0215fd039c91629d2d1a29cb777535ea534c
plutus <914185324@qq.com>
2025-03-18 17:54:03 +0800

    条件渲染

--- src/components/IfDemo.vue
+++ src/components/IfDemo.vue
@@ -1,7 +1,22 @@
 <template>
     <h3>条件渲染</h3>
+    <div v-if="flag">CAN YOU SEE ME</div>
+    <div v-else>see me,thanks</div>
+    <div v-if="type === 'A'">A</div>
+    <div v-else-if="type === 'B'">B</div>
+    <div v-else-if="type === 'C'">C</div>
+    <div v-else>NOT A/B/C</div>
+    <div v-show="flag">v-show</div>
 </template>
 
-<script>
 
+<script>
+export default {
+    data(){
+        return{
+            flag:false,
+            type:"D"
+        }
+    }
+}
 </script>
