5b273182181ba98b2f3b8ef789f2c35aa1531b27
plutus <914185324@qq.com>
2025-03-18 15:59:30 +0800

    PS2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
 <h1>模板语法</h1>
 <p>{{ msg }}</p>
+<p>{{ number + 1 }}</p>
+<p>{{ ok ? 'yes' : 'no ' }}</p>
+<p>{{ message.split('').reverse() }}</p>
+<!-- <p>{{ var = 1 }}</p> -->
+<!-- <p>{{ if(ok){return message} }}</p> -->
+ <p>{{ rawHtml }}</p>
+ <p v-html="rawHtml"></p>
+
 </template>
 
 <script>
 export default{
   data(){
     return{
-      msg:"神奇的语法"
+      msg:"神奇的语法",
+      number:10,ok:true,
+      message:"大家好",
+      rawHtml:"<a href='https://cn.vuejs.org/guide/quick-start.html'>vue<a/>"
     }
   }
 }
