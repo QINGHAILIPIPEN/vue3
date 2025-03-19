76d09e7ccd297be154e3ee6d987ce8b30a763df5
plutus <914185324@qq.com>
2025-03-19 10:50:44 +0800

    数组变化侦测

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,11 @@ import EventDemo3 from './components/EventDemo3.vue'
 import EventDemo4 from './components/EventDemo4.vue'
 import EventDemo5 from './components/EventDemo5.vue'
 import EventDemo6 from './components/EventDemo6.vue'
+import ArrayList from './components/ArrayList.vue'
 </script>
 
 <template>
-  <HelloWorld/>
+  <!-- <HelloWorld/>
   <IfDemo/>
   <ListDemo/>
   <keyDemo/>
@@ -21,5 +22,6 @@ import EventDemo6 from './components/EventDemo6.vue'
   <EventDemo3/>
   <EventDemo4/>
   <EventDemo5/>
-  <EventDemo6/>
+  <EventDemo6/> -->
+  <ArrayList/>
 </template>
---
+++ src/components/ArrayList.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>数组变化侦听</h1>
+    <button @click="addListHandle">添加数据</button>
+    <ul>
+        <li v-for="(item,index) of names" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="concatHandle">合并数组</button>
+    <h3>数组1</h3>
+    <p v-for="(item,index) of num1">{{ item }}</p>
+    <h3>数组2</h3>
+    <p v-for="(item,index) of num2">{{ item }}</p>
+
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            names:["iwen","ime","frank"],
+            num1:[1,2,3,4,5],
+            num2:[6,7,8,9,10]
+        }
+        
+    },
+    methods:{
+        addListHandle(){
+            //引起UI自动更新
+            //this.names.push("sakura")
+            //不会引起UI自动更新
+            this.names.concat(["sakura"])
+            //console.log(this.names.concat(["sakura"]));
+            this.names = this.names.concat(["sakura"])
+            
+        },
+        concatHandle(){
+           this.num1 = this.num1.concat(this.num2)
+        }
+    }
+}
+</script>
